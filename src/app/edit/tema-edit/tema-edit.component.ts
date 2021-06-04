7433a703a0153173ae6014a71d013a3ec4d1ede9
Anderson Correia de Souza <anderson_souza33@outlook.com.br>
2021-06-04 09:54:50 -0300

    feat: tema especiífico para começar a editar

--- src/app/edit/tema-edit/tema-edit.component.ts
+++ src/app/edit/tema-edit/tema-edit.component.ts
@@ -1,4 +1,8 @@
+import { environment } from './../../../environments/environment.prod';
+import { TemaService } from './../../service/tema.service';
 import { Component, OnInit } from '@angular/core';
+import { Tema } from 'src/app/model/Tema';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-tema-edit',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemaEditComponent implements OnInit {
 
-  constructor() { }
+  tema: Tema = new Tema()
 
-  ngOnInit(): void {
+  constructor(
+    private temaService: TemaService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(){
+    if(environment.token == ''){
+      alert("Sua sessão expirou, faça o login novamente");
+      this.router.navigate(['/entrar']);
+    }
+
+    let id = this.route.snapshot.params['id']
+    this.findByIdTema(id)
   }
+
+  findByIdTema(id: number){
+    return this.temaService.getByIdTema(id).subscribe((resp: Tema) => {
+      this.tema = resp
+    })
+  }
+
+  
 
 }
