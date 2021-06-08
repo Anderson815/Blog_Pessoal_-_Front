4668c22d6d4238060fab4a7c4e1cdec1fe784a99
Anderson Correia de Souza <anderson_souza33@outlook.com.br>
2021-06-08 14:41:07 -0300

    feat: indentifica a postagem no ts de postagem-edit

--- src/app/edit/postagem-edit/postagem-edit.component.ts
+++ src/app/edit/postagem-edit/postagem-edit.component.ts
@@ -1,4 +1,8 @@
+import { ActivatedRoute, Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
 import { Component, OnInit } from '@angular/core';
+import { Postagem } from 'src/app/model/Postagem';
+import { PostagemService } from 'src/app/service/postagem.service';
 
 @Component({
   selector: 'app-postagem-edit',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostagemEditComponent implements OnInit {
 
-  constructor() { }
+  postagem: Postagem = new Postagem();
 
-  ngOnInit(): void {
+  constructor(
+    private postagemService: PostagemService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(){
+    if(environment.token == ""){
+      alert("Sua sessão expirou, faça o login novamente");
+      this.router.navigate(['/entrar']);
+    }
+
+    let id = this.route.snapshot.params['id'];
+    this.findByIdPostagem(id)
   }
+
+  findByIdPostagem(id: number){
+    this.postagemService.getByIdPostagem(id).subscribe((resp: Postagem) => {
+      this.postagem = resp
+    })
+  }
+
+
 
 }
