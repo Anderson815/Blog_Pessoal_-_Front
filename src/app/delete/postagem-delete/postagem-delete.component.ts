080861825f0b2594bc8c5395720eb689a8b97ba8
Anderson Correia de Souza <anderson_souza33@outlook.com.br>
2021-06-08 19:46:45 -0300

    feat: preparo do ts para deletar postagem

--- src/app/delete/postagem-delete/postagem-delete.component.ts
+++ src/app/delete/postagem-delete/postagem-delete.component.ts
@@ -1,4 +1,8 @@
+import { environment } from './../../../environments/environment.prod';
+import { PostagemService } from './../../service/postagem.service';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { Postagem } from 'src/app/model/Postagem';
 
 @Component({
   selector: 'app-postagem-delete',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostagemDeleteComponent implements OnInit {
 
-  constructor() { }
+  idPostagem: number
+  postagem: Postagem
 
-  ngOnInit(): void {
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private postagemService: PostagemService
+  ) { }
+
+  ngOnInit(){
+
+    window.scroll(0, 0);
+
+    if(environment.token == ""){
+      alert("Sua sessão expirou, faça o login novamente");
+      this.router.navigate(['/entrar']);
+    }
+
+    this.idPostagem = this.route.snapshot.params['id'];
+    this.findByIdPostagem(this.idPostagem)
   }
 
+  findByIdPostagem(id: number){
+    this.postagemService.getByIdPostagem(id).subscribe((resp: Postagem) => {
+      this.postagem = resp;
+    })
+  }
+
+  
 }
