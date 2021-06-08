d9728a36da5e8036782a2f9000b1847be48ab624
Anderson Correia de Souza <anderson_souza33@outlook.com.br>
2021-06-08 20:19:44 -0300

    feat: rota do user-edit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { UserEditComponent } from './edit/user-edit/user-edit.component';
 import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delete.component';
 import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.component';
 import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
@@ -23,7 +24,9 @@ const routes: Routes = [
   {path: 'tema-delete/:id', component: TemaDeleteComponent},
 
   {path: 'postagem-edit/:id', component: PostagemEditComponent},
-  {path: 'postagem-delete/:id', component: PostagemDeleteComponent}
+  {path: 'postagem-delete/:id', component: PostagemDeleteComponent},
+
+  {path: 'user-edit/:id', component: UserEditComponent}
 ];
 
 @NgModule({
