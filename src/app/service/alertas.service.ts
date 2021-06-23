c8451bedc64a3a73f2e8bef912df478d9622d685
Anderson Correia de Souza <anderson_souza33@outlook.com.br>
2021-06-23 17:32:15 -0300

    feat: métodos de service do alerta

--- src/app/service/alertas.service.ts
+++ src/app/service/alertas.service.ts
@@ -1,9 +1,31 @@
+import { AlertasComponent } from './../alertas/alertas.component';
 import { Injectable } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlertasService {
 
-  constructor() { }
+  constructor(
+    private bsModalService: BsModalService
+  ) { }
+
+  private showAlert(msg: string, tipo: string){
+    const bsModalRef: BsModalRef = this.bsModalService.show(AlertasComponent);
+    bsModalRef.content.type = tipo;
+    bsModalRef.content.message = msg;
+  }
+
+  showAlertDanger(msg: string){
+    this.showAlert(msg, 'danger')
+  }
+
+  showAlertSuccess(msg: string){
+    this.showAlert(msg, 'success')
+  }
+
+  showAlertInfo(msg: string){
+    this.showAlert(msg, 'info')
+  }
 }
